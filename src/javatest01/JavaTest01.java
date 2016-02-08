f37dad77f503a900671aa6c9ff69c9b914955db4
keith <keith@JKM04>
2016-02-08 20:15:58 +0000

    Second commit Linux

---
+++ src/javatest01/JavaTest01.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatest01;
+
+/**
+ *
+ * @author keith
+ */
+
+
+public class JavaTest01{
+
+public static void main(String []args){
+
+System.out.println("Happy Birthday Keith");
+
+
+}
+
+}
